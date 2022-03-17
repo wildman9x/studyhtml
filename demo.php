1734dc058b7f22042dd5067140394d3b7c80bb06
wildman9x <wildmanyt@gmail.com>
2022-03-17 00:44:23 -0700

    Update demo.php

--- demo.php
+++ demo.php
@@ -3,11 +3,12 @@
 <head>
 <meta charset="utf-8">
 <title>Hello World</title>
-<script type="text/javascript"></script>
-function displayTime() {
+<script type="text/javascript">function displayTime() {
 var d = document.getElementById("demo");
 d.innerHTML = " " + Date();
 }
+</script>
+
 </head>
 <body onload="displayTime()">
     <!--Print hello world and the current date and time and second-->
