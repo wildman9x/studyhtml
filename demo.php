270ed826067d16d14f982ebbc2cd6af93cd9bbb4
wildman9x <wildmanyt@gmail.com>
2022-03-17 13:57:32 +0700

    Add files via upload

---
+++ demo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Hello World</title>
+</head>
+<body>
+    <!--Print hello world and the current date and time and second-->
+<?php
+    echo "Hello World!<br>";
+    echo "The date is " . date("Y-m-d") . "<br>";
+    echo "The time is " . date("h:i:sa") . "<br>";
+    echo "The second is " . date("s") . "<br>";
+?>
+</body>
+</html>
